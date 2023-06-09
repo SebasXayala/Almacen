5020dd3ad52756f3331d1862c1648ab0acc9f143
SebasXayala <juansebastianayala48@gmail.com>
2023-06-09 18:46:30 -0500

    se agrego los atributos constructores getter y setters de la clase venta

--- src/modelo/Venta.java
+++ src/modelo/Venta.java
@@ -11,4 +11,47 @@ package modelo;
  */
 public class Venta {
     
+    private int id;
+    private Producto producto;
+    private Cliente cliente;
+    private int cantidad;
+
+    public Venta(int id, Producto producto, Cliente cliente, int cantidad) {
+        this.id = id;
+        this.producto = producto;
+        this.cliente = cliente;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    } 
 }
