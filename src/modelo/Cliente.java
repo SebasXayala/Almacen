5d1b7ccb60242a79aa4da781b873ca55f58bc610
SebasXayala <juansebastianayala48@gmail.com>
2023-06-09 18:11:53 -0500

    se agrego los atributos, constructor, getters y setters

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -11,4 +11,70 @@ package modelo;
  */
 public class Cliente {
     
+    private int id;
+    private int cedula;
+    private String nombre;
+    private String apellido;
+    private String genero;
+    private String estado;
+
+    public Cliente(int id, int cedula, String nombre, String apellido, String genero, String estado) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
 }
